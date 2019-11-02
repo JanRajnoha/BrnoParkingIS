827bd3e1fa93c4a59b7822988f89a173de6bced5
Jan Rajnoha <janrajnoha@outlook.com>
2019-11-02 16:18:01 +0100

    Init implementation - Shell, MainPage, MainPageVM

--- BrnoParkingIS/App.xaml.cs
+++ BrnoParkingIS/App.xaml.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using BrnoParkingIS.Modules;
+using BrnoParkingIS.Views;
+using ISUF.Base.Classes;
+using ISUF.Storage.DatabaseAccess;
+using ISUF.Storage.Manager;
+using ISUF.UI.App;
+using ISUF.UI.Controls;
+using ISUF.UI.Modules;
+using ISUF.UI.Views;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Template10.Common;
+using Template10.Controls;
+using Template10.Services.NavigationService;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Core;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -20,16 +36,33 @@ namespace BrnoParkingIS
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    sealed partial class App : Application
+    public sealed partial class App : ApplicationBase
     {
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
         /// executed, and as such is the logical equivalent of main() or WinMain().
         /// </summary>
-        public App()
+        public App() : base(Package.Current.DisplayName + (Constants.Instance.BetaMode ? " - Beta version" : ""), typeof(Shell), typeof(MainPage), null)
         {
-            this.InitializeComponent();
-            this.Suspending += OnSuspending;
+            InitializeComponent();
+        }
+
+        public override void RegisterModules()
+        {
+            ModuleManager = new UIModuleManager(typeof(XmlDbAccess));
+
+            var carModule = new UIModule(typeof(Car), typeof(BaseItemManager), "Cars", (Symbol)0xE711, null);
+            var parkingModule = new UIModule(typeof(Parkings), typeof(BaseItemManager), "Parkings", Symbol.BackToWindow, null);
+
+            ModuleManager.RegisterModule(carModule);
+            ModuleManager.RegisterModule(parkingModule);
+        }
+
+        public override async Task OnInitializeAsync(IActivatedEventArgs args)
+        {
+            await base.OnInitializeAsync(args);
+
+            await Task.CompletedTask;
         }
 
         /// <summary>
@@ -78,7 +111,7 @@ namespace BrnoParkingIS
         /// </summary>
         /// <param name="sender">The Frame which failed navigation</param>
         /// <param name="e">Details about the navigation failure</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        protected void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
         {
             throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
         }
@@ -95,6 +128,12 @@ namespace BrnoParkingIS
             var deferral = e.SuspendingOperation.GetDeferral();
             //TODO: Save application state and stop any background activity
             deferral.Complete();
+        }
+
+        public override async Task OnStartAsync(StartKind startKind, IActivatedEventArgs args)
+        {
+            await NavigationService.NavigateAsync(typeof(MainPage));
+            await Task.CompletedTask;
         }
     }
 }
---
+++ BrnoParkingIS/Views/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using BrnoParkingIS.ViewModels;
+using ISUF.UI.Views;
+using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace BrnoParkingIS.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : MainPageBase
+    {
+        public MainPage() : base(typeof(MainPageViewModel))
+        {
+            InitializeComponent();
+            Loading += MainPageLoading;
+        }
+
+        private void MainPageLoading(FrameworkElement sender, object args)
+        {
+            mainPageMenu.ItemsSource = App.Current.AppUIModules;
+        }
+
+        //protected override void MainPage_Loading(FrameworkElement sender, object args)
+        //{
+        //    //throw new NotImplementedException();
+        //}
+    }
+}
