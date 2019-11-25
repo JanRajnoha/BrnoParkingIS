e3c684c094c8fbc9909ef8d0dc486e63c3f49c51
Jan Rajnoha <janrajnoha@outlook.com>
2019-11-25 23:29:25 +0100

    Add control implementation

---
+++ BrnoParkingIS/Controls/AddItem.xaml.cs
@@ -0,0 +1,29 @@
+﻿using ISUF.UI.Modules;
+using ISUF.UI.Views;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace BrnoParkingIS.Controls
+{
+    public sealed partial class AddItem : ModuleAddControlBase
+    {
+        public AddItem(UIModule uiModule, Type viewModelType, params object[] viewModelArgs) : base(uiModule, viewModelType, viewModelArgs)
+        {
+            this.InitializeComponent();
+        }
+    }
+}
--- BrnoParkingIS/ViewModels/CarViewModel.cs
+++ BrnoParkingIS/ViewModels/CarViewModel.cs
@@ -1,3 +1,4 @@
+using BrnoParkingIS.Controls;
 using BrnoParkingIS.Modules;
 using ISUF.Base.Messages;
 using ISUF.UI.ViewModel;
@@ -7,6 +8,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Windows.UI.StartScreen;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 namespace BrnoParkingIS.ViewModels
 {
@@ -27,7 +31,75 @@ namespace BrnoParkingIS.ViewModels
 
         protected override void AddPane<TMessage>(string paneName, TMessage msg)
         {
-            throw new NotImplementedException();
+
+            string header = "";
+            object content;
+
+            switch (paneName)
+            {
+                case addPivotItemName:
+
+                    content = new AddItem(uiModule, typeof(CarAddViewModel), null);
+
+                    if (msg.GetType() == typeof(ItemAddNewMsg))
+                    {
+                        header = "Add Activity";
+                    }
+                    else
+                    {
+                        header = "Edit Activity";
+                    }
+
+                    break;
+
+                default:
+                    throw new NotImplementedException();
+            }
+
+            try
+            {
+                if (PivotPanes.FirstOrDefault(x => x.Name == paneName) == null)
+                    PivotPanes.Insert(0, new PivotItem()
+                    {
+                        Name = paneName,
+                        Header = header,
+                        Margin = new Thickness(0),
+                        Content = content
+                    });
+                else
+                {
+                    PivotPanes.FirstOrDefault(x => x.Name == paneName).Header = header;
+                    PivotPanes.FirstOrDefault(x => x.Name == paneName).Content = content;
+                }
+            }
+            catch (Exception)
+            {
+                PivotPanes.Clear();
+                PivotPanes.Insert(0, new PivotItem()
+                {
+                    Name = paneName,
+                    Header = header,
+                    Content = content
+                });
+            }
+            finally
+            {
+                if (PivotPanes.FirstOrDefault(x => x.Name == paneName) != null)
+                {
+                    //pivotPanes = PivotPanes;
+                    RaisePropertyChanged(nameof(PivotPanes));
+
+                    PaneVisibility = true;
+
+                    double MinNormalWidth = (double)Application.Current.Resources["NormalMinWidth"];
+                    if (MinNormalWidth > ApplicationView.GetForCurrentView().VisibleBounds.Width)
+                    {
+                        MasterFrame = new GridLength(0);
+                    }
+
+                    Messenger.Send(msg);
+                }
+            }
         }
 
         protected override void NewItemAdded(ItemAddSavedMsg obj)
